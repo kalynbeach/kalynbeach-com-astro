186b73e054791b198ba6b2762f023d44a8f0fc5b
kalynbeach <kalynbeach@gmail.com>
2023-01-17 13:10:18 -0800

    add @astrojs/image, replace img with Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import image from "@astrojs/image";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
@@ -9,12 +10,15 @@ export default defineConfig({
   site: 'https://kalynbeach-com.netlify.app',
   integrations: [
     mdx(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
     sitemap(),
     tailwind(),
     partytown({
       config: {
-        forward: ["dataLayer.push"],
-      },
+        forward: ["dataLayer.push"]
+      }
     })
   ]
 });
