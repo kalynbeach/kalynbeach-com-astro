ce58856cb15a6aa5149fdd13d9f96c1949b5bb2e
kalynbeach <kalynbeach@gmail.com>
2023-02-03 14:34:48 -0800

    replace tabs with spaces

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,12 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
 export async function get(context) {
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		// site: import.meta.env.SITE,
-		site: context.site,
-		items: await pagesGlobToRssItems(
-			import.meta.glob('./blog/**/*.{md,mdx}')
-		),
-	});
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./blog/**/*.{md,mdx}')
+    ),
+  });
 }
