121d2e8030af45bea804ab5573a62952d8b794b3
kalynbeach <kalynbeach@gmail.com>
2023-03-02 17:39:16 -0800

    refactor for astro content collections support

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    draft: z.boolean().optional(),
+		title: z.string(),
+		description: z.string(),
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
